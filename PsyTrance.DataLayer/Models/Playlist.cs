d21594da413773ea66b50c111c9e0f46508a29cf
vjacheslavravdin <vjacheslavravdin@me.com>
2013-09-04 17:12:56 +0400

    17:12 04.09.2013

--- PsyTrance.DataLayer/Models/Playlist.cs
+++ PsyTrance.DataLayer/Models/Playlist.cs
@@ -1,7 +1,26 @@
-﻿namespace PsyTrance.DataLayer.Models
+﻿using System.Collections.Generic;
+
+namespace PsyTrance.DataLayer.Models
 {
     public class Playlist
     {
+        private List<Album> _albums;
+        private List<Song> _songs;
+
         public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public virtual List<Album> Albums
+        {
+            get { return _albums ?? (new List<Album>()); }
+            set { _albums = value; }
+        }
+
+        public virtual List<Song> Songs
+        {
+            get { return _songs ?? (new List<Song>()); }
+            set { _songs = value; }
+        }
     }
 }
--- PsyTrance.DataLayer/Models/User.cs
+++ PsyTrance.DataLayer/Models/User.cs
@@ -1,10 +1,31 @@
-﻿namespace PsyTrance.DataLayer.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace PsyTrance.DataLayer.Models
 {
-    public class User
+    public class User : IEquatable<User>
     {
+        private List<Playlist> _playlists;
+
         public int Id { get; set; }
 
         public string Name { get; set; }
         public string Password { get; set; }
+
+        public virtual List<Playlist> Playlists
+        {
+            get { return _playlists ?? (new List<Playlist>()); }
+            set { _playlists = value; }
+        }
+
+        public bool Equals(User user)
+        {
+            return Name.Equals(user.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
     }
 }
