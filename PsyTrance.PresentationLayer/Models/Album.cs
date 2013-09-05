f371c2fd374c8bc5085a92455ccc9f514544b3c9
vjacheslavravdin <vjacheslavravdin@me.com>
2013-09-06 03:00:09 +0400

    3:00 06.09.2013

--- PsyTrance.PresentationLayer/Models/Album.cs
+++ PsyTrance.PresentationLayer/Models/Album.cs
@@ -7,5 +7,12 @@ namespace PsyTrance.PresentationLayer.Models
 {
     public class Album
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public List<Artist> Artists { get; set; }
+        public List<Genre> Genres { get; set; }
+        public List<Song> Songs { get; set; }
     }
 }
