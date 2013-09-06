7458a979d7ef9bad6624559f75d664e02e04c1b3
vjacheslavravdin <vjacheslavravdin@me.com>
2013-09-06 11:28:28 +0400

    11:28 06.09.2013

--- PsyTrance.DataLayer/Models/Playlist.cs
+++ PsyTrance.DataLayer/Models/Playlist.cs
@@ -4,23 +4,11 @@ namespace PsyTrance.DataLayer.Models
 {
     public class Playlist
     {
-        private List<Album> _albums;
-        private List<Song> _songs;
-
         public int Id { get; set; }
 
         public string Title { get; set; }
 
-        public virtual List<Album> Albums
-        {
-            get { return _albums ?? (new List<Album>()); }
-            set { _albums = value; }
-        }
-
-        public virtual List<Song> Songs
-        {
-            get { return _songs ?? (new List<Song>()); }
-            set { _songs = value; }
-        }
+        public virtual List<Album> Albums { get; set; }
+        public virtual List<Song> Songs { get; set; }
     }
 }
--- PsyTrance.DataLayer/Models/User.cs
+++ PsyTrance.DataLayer/Models/User.cs
@@ -5,18 +5,11 @@ namespace PsyTrance.DataLayer.Models
 {
     public class User : IEquatable<User>
     {
-        private List<Playlist> _playlists;
-
         public int Id { get; set; }
 
         public string Name { get; set; }
-        public string Password { get; set; }
 
-        public virtual List<Playlist> Playlists
-        {
-            get { return _playlists ?? (new List<Playlist>()); }
-            set { _playlists = value; }
-        }
+        public virtual List<Playlist> Playlists { get; set; }
 
         public bool Equals(User user)
         {
