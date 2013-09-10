6378f098e6a31ead97d2909379ed28c952cd1540
vjacheslavravdin <vjacheslavravdin@me.com>
2013-09-10 23:33:39 +0400

    23:33 10.09.2013

--- PsyTrance.ServiceLayer/Controllers/HomeController.cs
+++ PsyTrance.ServiceLayer/Controllers/HomeController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PsyTrance.DataLayer;
 
 namespace PsyTrance.ServiceLayer.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
         public HomeController()
         {
+            _unitOfWork = new UnitOfWork();
         }
 
         public ActionResult Index()
