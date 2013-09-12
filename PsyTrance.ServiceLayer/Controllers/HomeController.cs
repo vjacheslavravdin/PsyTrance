f7c07ed7fa2f1ba1e7bf5f75288defe3f46b26f7
vjacheslavravdin <vjacheslavravdin@me.com>
2013-09-12 19:57:42 +0400

    19:57 12.09.2013

--- PsyTrance.ServiceLayer/Controllers/HomeController.cs
+++ PsyTrance.ServiceLayer/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using PsyTrance.DataLayer;
 using PsyTrance.DataLayer.Models;
+using WebGrease.Css.Extensions;
 
 namespace PsyTrance.ServiceLayer.Controllers
 {
@@ -21,52 +22,51 @@ namespace PsyTrance.ServiceLayer.Controllers
 
         public void I()
         {
-            var directories = System.IO.Directory.EnumerateDirectories(@"C:\Albums").ToList();
+            var directories = System.IO.Directory.EnumerateDirectories("").ToList();
 
-            if (directories.Count == 0)
-                throw new Exception();
-
-            foreach (var directory in directories)
+            if (directories.Count > 0)
             {
-                var files = System.IO.Directory.EnumerateFiles(directory).ToList();
-
-                if (files.Count == 0)
-                    throw new Exception();
-
-                var unitOfWork = new UnitOfWork();
-
-                var albumArtists = unitOfWork.AlbumArtistsRepository.Select();
-                var albums = unitOfWork.AlbumsRepository.Select();
-                var artists = unitOfWork.ArtistsRepository.Select();
-                var genres = unitOfWork.GenresRepository.Select();
-                var songs = unitOfWork.SongsRepository.Select();
-
-                foreach (var file in files)
+                foreach (var directory in directories)
                 {
-                    using (var file = TagLib.File.Create(path))
+                    var files = System.IO.Directory.EnumerateFiles("").ToList();
+
+                    if (files.Count > 0)
                     {
-                        var albumArtists = file.Tag.AlbumArtists.Select(title => new AlbumArtist
+                        using (var unitOfWork = new UnitOfWork())
                         {
-                            Title = title,
-                            Albums = new List<Album>(),
-                            Artists = new List<Artist>(),
-                            Genres = new List<Genre>(),
-                            Songs = new List<Song>()
-                        }).ToList();
+                            var albumArtists = unitOfWork.AlbumArtistsRepository.Select();
+                            var albums = unitOfWork.AlbumsRepository.Select();
+                            var artists = unitOfWork.ArtistsRepository.Select();
+                            var genres = unitOfWork.GenresRepository.Select();
+                            var songs = unitOfWork.SongsRepository.Select();
 
-                        _albumArtists = _albumArtists.Union(albumArtists).ToList();
+                            foreach (var file in files)
+                            {
+                                using (var file = TagLib.File.Create(path))
+                                {
+                                    var albumArtists = file.Tag.AlbumArtists.Select(title => new AlbumArtist
+                                    {
+                                        Title = title,
+                                        Albums = new List<Album>(),
+                                        Artists = new List<Artist>(),
+                                        Genres = new List<Genre>(),
+                                        Songs = new List<Song>()
+                                    }).ToList();
 
-                        _albumArtists.Intersect(albumArtists).ToList().ForEach(delegate(AlbumArtist albumArtist)
-                        {
-                            albumArtist.Albums.AddRange(_albums.Intersect(albums).Except(albumArtist.Albums).ToList());
-                            albumArtist.Artists.AddRange(_artists.Intersect(artists).Except(albumArtist.Artists).ToList());
-                            albumArtist.Genres.AddRange(_genres.Intersect(genres).Except(albumArtist.Genres).ToList());
-                            albumArtist.Songs.AddRange(_songs.Intersect(songs).Except(albumArtist.Songs).ToList());
-                        });
+                                    _albumArtists = _albumArtists.Union(albumArtists).ToList();
+
+                                    _albumArtists.Intersect(albumArtists).ToList().ForEach(delegate(AlbumArtist albumArtist)
+                                    {
+                                        albumArtist.Albums.AddRange(_albums.Intersect(albums).Except(albumArtist.Albums).ToList());
+                                        albumArtist.Artists.AddRange(_artists.Intersect(artists).Except(albumArtist.Artists).ToList());
+                                        albumArtist.Genres.AddRange(_genres.Intersect(genres).Except(albumArtist.Genres).ToList());
+                                        albumArtist.Songs.AddRange(_songs.Intersect(songs).Except(albumArtist.Songs).ToList());
+                                    });
+                                }
+                            }
+                        }
                     }
                 }
-
-                unitOfWork.Dispose();
             }
         } 
     }
